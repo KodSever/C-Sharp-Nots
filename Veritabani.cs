6916698604f16df45bac40b18abf84895780447a
Yiğit KARAKOYUN <38832410+KodSever@users.noreply.github.com>
2019-06-04 01:05:45 +0300

    Update Veritabani.cs

--- Veritabani.cs
+++ Veritabani.cs
@@ -1,3 +1,21 @@
+//DATAREADER2
+OleDbConnection baglan = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=KISIADRESLER.accdb;Persist Security Info = False;");
+private void button1_Click(object sender, EventArgs e)
+{
+  baglan.Open();
+  listBox1.Items.Clear();
+  OleDbCommand komut = new OleDbCommand("Select * from Kisiler", baglan);
+  OleDbDataReader oku = komut.ExecuteReader();
+  while (oku.Read()) 
+  {
+    listBox1.Items.Add(
+    oku["ID"] + "-" + 
+    oku["AD"] + " " + 
+    oku["SOYAD"]);
+  }
+  baglan.Close();
+}
+
 //DATAADAPTOR
 OleDbConnection baglan = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=KISIADRESLER.accdb;Persist Security Info = False;");
 private void button1_Click(object sender, EventArgs e)
