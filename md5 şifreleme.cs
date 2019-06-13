8f9623145f2997f0ac7bb0b115ee48851c4cdd99
Yiğit KARAKOYUN <38832410+KodSever@users.noreply.github.com>
2019-06-13 16:07:03 +0300

    Update md5 şifreleme.cs

--- md5 şifreleme.cs
+++ md5 şifreleme.cs
@@ -2,14 +2,14 @@
 //MD5 ŞİFRELEME
 using System.Security.Cryptography;
 public static string MD5(string _metin)
-    {
-        MD5CryptoServiceProvider pwd = new MD5CryptoServiceProvider();
-        return encryption(_metin, pwd);
-    }
+{
+    MD5CryptoServiceProvider pwd = new MD5CryptoServiceProvider();
+    return encryption(_metin, pwd);
+}
 
 private static string encryption(string _metin, HashAlgorithm _algoritma)
-    {
-        byte[] byteDegeri = System.Text.Encoding.UTF8.GetBytes(_metin);
-        byte[] sifreliByte = _algoritma.ComputeHash(byteDegeri);
-        return Convert.ToBase64String(sifreliByte);
-    }
+{
+    byte[] byteDegeri = System.Text.Encoding.UTF8.GetBytes(_metin);
+    byte[] sifreliByte = _algoritma.ComputeHash(byteDegeri);
+    return Convert.ToBase64String(sifreliByte);
+}
